d5cc4def572d5660009408b006d920c6375fca66
suwan <skdoqj@naver.com>
2023-12-13 00:48:06 +0900

    catImg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import Image from "next/image";
+import catImg from "/public/cat1.png";
+import { useState } from "react";
 
 export default function Home() {
-  return <>Hello Next</>;
+  const [answer, setAnswer] = useState();
+
+  return (
+    <>
+      <h1>Cheater Cat</h1>
+      {/* <img src="/cat1.png" alt="Cheater cat" /> */}
+      <Image src={catImg} alt="cheater cat" height="200" />
+      <form>
+        <input type="text"></input>
+        <button>보내기</button>
+      </form>
+    </>
+  );
 }
